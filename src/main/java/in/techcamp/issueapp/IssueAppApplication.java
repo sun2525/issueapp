3d164f4797eba4ea6cfb647efc1e9870cddfb795
sun2525 <tooooku88@gmail.com>
2025-05-02 10:30:33 +0900

    投稿機能

--- src/main/java/in/techcamp/issueapp/IssueAppApplication.java
+++ src/main/java/in/techcamp/issueapp/IssueAppApplication.java
@@ -1,13 +1,14 @@
 package in.techcamp.issueapp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("in.techcamp.issueapp")  // この行を追加
 public class IssueAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IssueAppApplication.class, args);
 	}
-
 }
