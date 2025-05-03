d6d1e8f4619b95ab1d10786a1e3d08601222059b
sun2525 <tooooku88@gmail.com>
2025-05-04 00:43:57 +0900

    コントローラーの変更

--- src/main/java/in/techcamp/issueapp/IssueController.java
+++ src/main/java/in/techcamp/issueapp/IssueController.java
@@ -1,6 +1,7 @@
 package in.techcamp.issueapp;
 
 import lombok.AllArgsConstructor;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,20 +13,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class IssueController {
 
     private final IssueRepository issueRepository;
+
     @GetMapping("/issueForm")
     public String showIssueForm(@ModelAttribute("issueForm") IssueForm form){
         return "issueForm";
     }
 
     @PostMapping("/issues")
-    public String createIssue(IssueForm issueForm,
-                              Model model){
-        try {
-            issueRepository.insert(issueForm.getTitle(), issueForm.getContent(), issueForm.getPeriod(), issueForm.getImportance());
-        } catch (Exception e) {
-            model.addAttribute("errorMessage", e.getMessage());
-            return "error";
-        }
+    public String createIssue(IssueForm issueForm){
+        issueRepository.insert(issueForm.getTitle(), issueForm.getContent(), issueForm.getPeriod(), issueForm.getImportance());
         return "redirect:/";
+    }
+
+    @GetMapping
+    public String showIssues(Model model){
+        var issueList = issueRepository.findAll();
+        model.addAttribute("issueList", issueList);
+        return "index";
     }
 }
